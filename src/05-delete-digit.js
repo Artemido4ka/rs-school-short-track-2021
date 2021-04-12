157e19f16016764c9096ec366291797778125191
Artemido4ka <korenko.artem9@gmail.com>
2021-04-12 23:54:48 +0300

    feat: delete-digit

--- src/05-delete-digit.js
+++ src/05-delete-digit.js
@@ -9,8 +9,21 @@
  * For n = 152, the output should be 52
  *
  */
-function deleteDigit(/* n */) {
-  throw new Error('Not implemented');
+function deleteDigit(n) {
+  let min = 1000;
+  const array = n.toString().split('');
+  for (let i = 0; i < array.length; i++) {
+    if (+array[i] < min) {
+      min = +array[i];
+    }
+  }
+  for (let i = 0; i < array.length; i++) {
+    if (+array[i] === min) {
+      array.splice(i, 1);
+    }
+  }
+
+  return +array.join('');
 }
 
 module.exports = deleteDigit;
